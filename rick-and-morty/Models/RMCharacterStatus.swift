b1c339c3039863db83ff0d8fac921d51b602f23d
wallefb <wallef.borges@image-plus.co.uk>
2023-05-02 17:29:30 +0100

    Putting together all models

---
+++ rick-and-morty/Models/RMCharacterStatus.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+enum RMCharacterStatus: String, Codable {
+    case alive = "Alive"
+    case dead = "Dead"
+    case `unknown` = "unknown"
+}
--- rick-and-morty/Models/RMLocation.swift
+++ rick-and-morty/Models/RMLocation.swift
@@ -1,1 +1,11 @@
 import Foundation
+
+struct RMLocation: Codable {
+    let id: Int
+    let name: String
+    let type: String
+    let dimension: String
+    let residents: [String]
+    let url: String
+    let created: String
+}
